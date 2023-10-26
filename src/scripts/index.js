2d8ffd25dd0b03a717a56c83a6166b9b6669c539
Víctor Castillo <Castillovictor2461@gmail.com>
2023-10-25 21:44:57 -0400

    finalizacion de las primeras 3 paginas completas

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,33 +1,25 @@
+document.getElementById('open-modal').addEventListener('click', function () {
+    document.getElementById('modal').style.display =
+        'block' /* Muestra el modal */
+})
+
+document.getElementById('close-modal').addEventListener('click', function () {
+    document.getElementById('modal').style.display =
+        'none' /* Oculta el modal */
+})
+
 document
-    .getElementById('subscribe-button')
+    .getElementById('confirm-button')
     .addEventListener('click', function () {
-        // Recolecta los datos que deseas enviar al servidor, por ejemplo, un correo electrónico
-        const email = 'correo@ejemplo.com'
+        // Oculta el primer modal
+        document.getElementById('modal').style.display = 'none'
+        // Muestra el segundo modal
+        document.getElementById('success-modal').style.display = 'block'
+    })
 
-        // Configura los datos de la solicitud
-        const data = {
-            email: email,
-        }
-
-        fetch('URL_DEL_BACKEND', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data),
-        })
-            .then((response) => {
-                if (response.ok) {
-                    // La solicitud fue exitosa
-                    alert('¡Te has suscrito exitosamente!')
-                } else {
-                    // La solicitud falló
-                    alert(
-                        'Hubo un problema al suscribirse. Inténtalo de nuevo más tarde.'
-                    )
-                }
-            })
-            .catch((error) => {
-                console.error('Error:', error)
-            })
+document
+    .getElementById('close-success-modal')
+    .addEventListener('click', function () {
+        // Oculta el segundo modal
+        document.getElementById('success-modal').style.display = 'none'
     })
